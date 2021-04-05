f379523b55edc68be0702c92fda26333e302889f
tarekfouda <tarekfouda0@gmail.com>
2021-04-06 01:20:00 +0200

    oneSignal

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Tarek Fouda on 06/04/2021.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
